0eb606a460ca3654c970611aa17ad0e1be915d5a
lunalhx <lunalhx@gmail.com>
2025-05-26 11:13:00 +0800

    保存当前工作进度 - 更新登录页面和Vue配置

--- RuoYi-Vue-master/ruoyi-ui/vue.config.js
+++ RuoYi-Vue-master/ruoyi-ui/vue.config.js
@@ -7,7 +7,7 @@ function resolve(dir) {
 
 const CompressionPlugin = require('compression-webpack-plugin')
 
-const name = process.env.VUE_APP_TITLE || '若依管理系统' // 网页标题
+const name = process.env.VUE_APP_TITLE || '人工智能与算力技术实验室管理系统' // 网页标题
 
 const baseUrl = 'http://localhost:8080' // 后端接口
 
